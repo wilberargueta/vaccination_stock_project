ff2880431f9e8763cdbc1c76f0a1b5e67ad73dca
Wilber Argueta <wilber.argueta@protonmail.com>
2021-11-09 16:13:16 -0600

    fixed compile.sh path

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 
 cd interface/ && npm install && ng build --configuration=production
 cd ..
-cp -R interface/dist/ui/static admin/src/main/resource/
+cp -R interface/dist/ui/static admin/src/main/resources/
 cd admin && mvn clean package -DskipTests
 cd ..
